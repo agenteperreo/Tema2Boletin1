03805c9e4afeba4f81cdf441f4ebfb45b5cb4af7
igarcia <igarcar@iesnervion.es>
2022-10-04 12:08:54 +0200

    4/10/2022

--- Tema2Boletin1/src/tema2boletin1/Ejercicio1.java
+++ Tema2Boletin1/src/tema2boletin1/Ejercicio1.java
@@ -1,10 +1,30 @@
 package tema2boletin1;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Declaramos las variables
+		int num;
+		
+		//Abrimos el escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Pedimos el numero
+		System.out.print("Dime un numero para decirte si es par: ");
+		num=sc.nextInt();
+		
+		//Condicional para saber si es par o impar
+		if (num%2 == 0) { //Si el resto de el numero entre 2 es 0 es par
+			System.out.println("El numero " + num+ " es par");
+		}else { //Si el resto es mayor que 0 es inpar
+			System.out.println("El numero " + num+ " es inpar");
+		}
+		
+		sc.close();
+		
 	}
 
 }
