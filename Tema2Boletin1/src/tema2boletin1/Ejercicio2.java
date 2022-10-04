d691d35d1518d8184e30bf0a5774fb7d3cc90653
igarcia <igarcar@iesnervion.es>
2022-10-04 12:42:27 +0200

    4/10/2022

---
+++ Tema2Boletin1/src/tema2boletin1/Ejercicio2.java
@@ -0,0 +1,27 @@
+package tema2boletin1;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+
+		//Declaramos las variables
+		int num1, num2;
+		
+		//Abrimos el escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Pedimos los 2 numeros
+		System.out.print("Dime un numero: ");
+		num1 = sc.nextInt();
+		System.out.print("Dime un ultimo numero: ");
+		num2 = sc.nextInt();
+		
+		//Condicional par saber si son iguales
+		if ( num1 == num2 ) {
+			System.out.printf("");
+		}
+	}
+
+}
