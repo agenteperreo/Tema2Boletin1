1b04fcfe9442f54997ea89246699ad006642f07d
igarcia <igarcar@iesnervion.es>
2022-10-05 13:31:51 +0200

    05/10/2022

--- Tema2Boletin1/src/tema2boletin1/Ejercicio2.java
+++ Tema2Boletin1/src/tema2boletin1/Ejercicio2.java
@@ -6,21 +6,23 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) {
 
-		//Declaramos las variables
+		// Declaramos las variables
 		int num1, num2;
-		
-		//Abrimos el escaner
+
+		// Abrimos el escaner
 		Scanner sc = new Scanner(System.in);
-		
-		//Pedimos los 2 numeros
+
+		// Pedimos los 2 numeros
 		System.out.print("Dime un numero: ");
 		num1 = sc.nextInt();
 		System.out.print("Dime un ultimo numero: ");
 		num2 = sc.nextInt();
-		
-		//Condicional par saber si son iguales
-		if ( num1 == num2 ) {
-			System.out.printf("");
+
+		// Condicional par saber si son iguales
+		if (num1 == num2) {
+			System.out.printf("Los numeros %d y %d son iguales",num1,num2);
+		} else {
+			System.out.printf("Los numeros %d y %d son diferentes",num1,num2);
 		}
 	}
 
---
+++ Tema2Boletin1/src/tema2boletin1/Ejercicio3.java
@@ -0,0 +1,36 @@
+package tema2boletin1;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+	
+	public static void main(String[] args) {
+		
+		//Decalramos las variables
+		double num1;
+		
+		//Abrimos el escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Pedimos el numero
+		System.out.print("Dime un nunmero decimal entre 1 y -1: ");
+		num1 = sc.nextDouble();
+		
+		//Condicionales para saber si es un numero casi cero
+		if (num1>=1) {
+			System.out.printf("El numero %.2f no es casi-cero",num1);
+		}else {
+			if ( num1<=-1) {
+				System.out.printf("El numero %.2f no es casi-cero",num1);
+			}else {
+				if (num1==0) {
+					System.out.printf("El numero %.2f no es casi-cero",num1);
+				}else {
+					System.out.printf("El numero %.2f es casi-cero",num1);
+				}
+			}
+		}
+		
+	}
+
+}
